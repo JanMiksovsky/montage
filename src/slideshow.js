adf4c9b3a090c3b3eb942e5bc9a7949d7687e38c
Jan Miksovsky <jan@miksovsky.com>
2022-07-08 11:05:47 -0700

    Crossfade

--- src/slideshow.js
+++ src/slideshow.js
@@ -1,21 +1,38 @@
-// const slideshowIntervalSeconds = 5;
-const slideshowIntervalSeconds = 60;
+const slideshowIntervalSeconds = 10;
+// const slideshowIntervalSeconds = 60;
+const transitionIntervalSeconds = 2;
 
 const slideshow = window.location.pathname === "/slideshow.html";
 
-if (slideshow) {
-  setTimeout(nextSlide, slideshowIntervalSeconds * 1000);
+function fadeTransitionEnd() {
+  const fadingOut = !document.body.classList.contains("showContent");
+  if (fadingOut) {
+    nextSlide();
+  }
+}
+
+function fadeIn() {
+  console.log("fadeIn");
+  document.body.classList.toggle("showContent", true);
+}
+
+function fadeOut() {
+  console.log("fadeOut");
+  document.body.classList.toggle("showContent", false);
 }
 
 function nextSlide() {
   window.location.reload();
 }
 
-// const cancel = (event) => {
-//   event.stopPropagation();
-//   event.preventDefault();
-// };
+function pageLoad() {
+  fadeIn();
+  if (slideshow) {
+    const delayIntervalSeconds =
+      slideshowIntervalSeconds - transitionIntervalSeconds;
+    setTimeout(fadeOut, delayIntervalSeconds * 1000);
+  }
+}
 
-// window.addEventListener("keydown", cancel);
-// window.addEventListener("keyup", cancel);
-// window.addEventListener("keypress", cancel);
+window.addEventListener("load", pageLoad);
+document.body.addEventListener("transitionend", fadeTransitionEnd);
