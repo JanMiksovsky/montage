1a83ca9be9ed325ed70e3bcaaa2cebc4cc48f08a
Jan Miksovsky <jan@miksovsky.com>
2022-07-08 17:13:11 -0700

    Transition tweaks

--- src/slideshow.js
+++ src/slideshow.js
@@ -1,6 +1,7 @@
 const slideshowIntervalSeconds = 10;
 // const slideshowIntervalSeconds = 60;
-const transitionIntervalSeconds = 2;
+const fadeInDuration = 2;
+const fadeOutDuration = 2;
 
 const slideshow = window.location.pathname === "/slideshow.html";
 
@@ -12,12 +13,10 @@ function fadeTransitionEnd() {
 }
 
 function fadeIn() {
-  console.log("fadeIn");
   document.body.classList.toggle("showContent", true);
 }
 
 function fadeOut() {
-  console.log("fadeOut");
   document.body.classList.toggle("showContent", false);
 }
 
@@ -26,10 +25,15 @@ function nextSlide() {
 }
 
 function pageLoad() {
+  // Add durations to body style attribute.
+  let bodyStyleAttribute = document.body.getAttribute("style") ?? "";
+  bodyStyleAttribute += `; --fadeInDuration: ${fadeInDuration}s; --fadeOutDuration: ${fadeOutDuration}s;`;
+  document.body.setAttribute("style", bodyStyleAttribute);
+
   fadeIn();
   if (slideshow) {
     const delayIntervalSeconds =
-      slideshowIntervalSeconds - transitionIntervalSeconds;
+      slideshowIntervalSeconds - fadeInDuration - fadeOutDuration;
     setTimeout(fadeOut, delayIntervalSeconds * 1000);
   }
 }
