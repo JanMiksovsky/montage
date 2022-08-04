4d43a47e6bc47cb43fb946a6fe3ec4bd4b9a05f0
jan <jan@miksovsky.com>
2022-08-04 13:19:27 -0700

    Use absolute, hard-coded path to node for now

--- slideshow.sh
+++ slideshow.sh
@@ -1,2 +1,2 @@
 cd /Users/jan/Source/montage
-node node_modules/@explorablegraph/explorable/src/cli/cli.js "serve graphVirtual(src), 3000"
+/usr/local/bin/node node_modules/@explorablegraph/explorable/src/cli/cli.js "serve graphVirtual(src), 3000"
