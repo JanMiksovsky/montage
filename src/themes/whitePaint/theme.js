3b98dfb6572d34838a8a4ea388d9b4d83ce48bd4
Jan Miksovsky <jan@miksovsky.com>
2022-08-09 17:15:26 -0700

    Second theme

---
+++ src/themes/whitePaint/theme.js
@@ -0,0 +1,7 @@
+import Image from "../Image.js";
+import Label from "../Label.js";
+import Slide from "../Slide.js";
+
+customElements.define("montage-image", Image);
+customElements.define("montage-label", Label);
+customElements.define("montage-slide", Slide);
