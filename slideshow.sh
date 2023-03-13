e90fcef718a9dd9ef1ee5b8659847466d7c125ee
Jan Miksovsky <jan@miksovsky.com>
2023-03-13 16:06:20 -0700

    Try restarting slideshow server every night

--- slideshow.sh
+++ slideshow.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+# Kill any process already listening on port 3000, namely an earlier slideshow
+lsof -i :3000 | grep LISTEN | awk '{print $2}' | xargs kill -9
+
+# Start a new slideshow
 cd /Users/jan/Source/montage
 /usr/local/bin/node node_modules/@graphorigami/origami/src/cli/cli.js "serve graphVirtual(src), 3000"
